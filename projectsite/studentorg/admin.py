eed1450e68aee213835cbb283367307383748ebf
Ryan Jazer Prieto <prietoryanjazer@gmail.com>
2026-02-16 15:25:18 +0800

    Task B - ProgramAdmin(modified)

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -9,7 +9,7 @@ class CollegeAdmin(admin.ModelAdmin):
     search_fields = ("college_name",)
     list_filter = ("created_at",)
 
-admin.register(Program)
+@admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
     list_display = ("program_name", "college")
     search_fields = ("program_name", "college__college_name")
