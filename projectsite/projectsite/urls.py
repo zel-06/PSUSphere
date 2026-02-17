b1b5c21600993c70b1fba2b079f02cef9f060586
Ryan Jazer Prieto <prietoryanjazer@gmail.com>
2026-02-17 22:41:21 +0800

    modify urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -20,10 +20,7 @@ from studentorg.views import HomePageView
 from studentorg import views
 
 urlpatterns = [
-path("admin/", admin.site.urls),
-path('', views.HomePageView.as_view(), name='home'),
+    path("admin/", admin.site.urls),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
